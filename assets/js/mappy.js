d0b7b7d233d7971ea0710362b643014288cb07c6
Matt Titmuss <45769440+mattmarc44@users.noreply.github.com>
2019-04-25 15:11:49 +0100

    added a places search bar using google examples at the cost of moving my search bar, more to be done

--- assets/js/mappy.js
+++ assets/js/mappy.js
@@ -1,27 +1,73 @@
-function initMap(){
+function initMap() {
     //initiate map
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
-          zoom: 3,
-          center: {lat: 50.1163, lng: -122.9575}
-        });
-        
-    //setting up markers locations
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var locations = [
-        {lat: 50.1555, lng: -122.9999},
-        {lat: 51.1555, lng: -123.9999},
-        {lat: 52.1555, lng: -124.9999}
-        ];
-    
-    //setting up markers    
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
+        zoom: 3,
+        center: { lat: 50.1163, lng: -122.9575 }
     });
-    
+
+    //creating search location by tying it to an ID
+    var input = document.getElementById('searchInput');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    // This is as per google documentation it works by watching the current maps bounds and setting them to the search bar
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+
+
+    //from here its as per google example
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+
     // Add a marker clusterer to manage the markers. (pin image)
-        var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+    var markerCluster = new MarkerClusterer(map, markers,
+        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
